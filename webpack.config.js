424dfe181091510c0203f8993ea30958059ac01e
alexKwonIsAwesome <khwalex@gmail.com>
2016-11-01 15:23:26 +0900

    using label loader for webpack config:
    As webpack can't parse jsx, with babel-loader, parse them through react
    and get the output and run them on es2015.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 module.exports = {
-  entry: './public/app.js',
+  entry: './public/app.jsx',
   output: {
     path: __dirname,
     filename: './public/bundle.js'
   },
   resolve: {
     extensions: ['','.js','.jsx']
-  }
+  },
+  loaders: [
+    {
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }
+  ]
 };
