2a5cafeaea5f8c3cd52eeeccb73acdadfd5e6e00
alexKwonIsAwesome <khwalex@gmail.com>
2016-11-03 13:36:19 +0900

    update promise practice
    New example case of practicing promise.

--- example-promise.js
+++ example-promise.js
@@ -5,13 +5,13 @@ function getTempCallback (location, callback) {
   callback('City not found');
 }
 
-getTempCallback('Philadelphia', function(err, temp) {
-  if (err) {
-    console.log('error', err);
-  } else {
-    console.log('success', temp)
-  }
-});
+//getTempCallback('Philadelphia', function(err, temp) {
+//  if (err) {
+//    console.log('error', err);
+//  } else {
+//    console.log('success', temp)
+//  }
+//});
 
 function getTempPromise (location) {
   return new Promise(function (resolve, reject) {
@@ -22,8 +22,32 @@ function getTempPromise (location) {
   });
 }
 
-getTempPromise('Philadelphia').then(function (temp) {
-  console.log('promise success', temp);
+//getTempPromise('Philadelphia').then(function (temp) {
+//  console.log('promise success', temp);
+//}, function (err) {
+//  console.log('promise error', err);
+//})
+
+// Challenge
+
+function addPromise(a, b) {
+  return new Promise(function (resolve, reject) {
+    if (typeof a === 'number' && typeof b === 'number') {
+      resolve(a + b);
+    } else {
+      reject("A & B need to be numbers");
+    }
+  });
+}
+
+addPromise(2, 3).then(function (numb) {
+  console.log('promise success', numb);
 }, function (err) {
   console.log('promise error', err);
-})
+});
+
+addPromise("alex", 9).then(function (numb) {
+  console.log('this should not show up');
+}, function(err) {
+  console.log('this should apper', err);
+});
